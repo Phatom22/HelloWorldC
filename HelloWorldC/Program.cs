f5365772e2824b2a825c553a2edee8b440e4c83b
nomvula <nzshabangu95@gmail.com>
2023-09-11 09:22:17 +0200

    Add project files.

---
+++ HelloWorldC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldC
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+
+
+             Console.WriteLine("lazy monday");
+            Console.WriteLine("hello jefferson we are doing c sharp this week");
+            Console.ReadLine();
+        }
+    }
+}
